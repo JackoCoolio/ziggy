c96ca3ee4c077bd8ecf3ae0b36af8c8d5df5fac6
Jackson Wambolt <jtwambolt@gmail.com>
2025-07-06 18:14:56 -0500

    use zig2nix to build project

--- flake.nix
+++ flake.nix
@@ -4,27 +4,35 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
+    zig2nix = {
+      url = "github:Cloudef/zig2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = inputs@{ flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems =
         [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
-      perSystem = { pkgs, lib, config, ... }: {
+      perSystem = { inputs', system, lib, config, ... }:
+        let
+          zig-env = inputs.zig2nix.zig-env.${system} {
+            zig = inputs'.zig2nix.packages.zig-0_14_0;
+          };
+          inherit (zig-env) pkgs;
+        in {
         packages = {
-          ziggy = pkgs.stdenv.mkDerivation {
-            name = "ziggy";
-            version = "0.0.0";
+          ziggy = zig-env.package {
             src = ./.;
-            postPatch = ''
-              ln -s ${pkgs.callPackage ./deps.nix { }} $ZIG_GLOBAL_CACHE_DIR/p
-            '';
-            nativeBuildInputs = [ pkgs.zig.hook ];
+            zigBuildZon = ./build.zig.zon;
+            zigBuildZonLock = ./build.zig.zon2json-lock;
+
+            # note: name and version are pulled from build.zig.zon
           };
           default = config.packages.ziggy;
           update-deps = pkgs.writeShellApplication {
             name = "update-deps";
-            text = "${lib.getExe pkgs.zon2nix} > deps.nix";
+            text = "${lib.getExe zig-env.zig2nix} zon2lock ./build.zig.zon";
           };
         };
         devShells.default = pkgs.mkShell {
